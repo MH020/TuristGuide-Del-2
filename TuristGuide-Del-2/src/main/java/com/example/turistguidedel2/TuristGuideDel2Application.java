e2482d62de76ab857a6a6ffa9ee5382af44680fa
MH020 <166133291+MH020@users.noreply.github.com>
2024-09-13 16:17:07 +0200

    And so it begins

---
+++ TuristGuide-Del-2/src/main/java/com/example/turistguidedel2/TuristGuideDel2Application.java
@@ -0,0 +1,13 @@
+package com.example.turistguidedel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristGuideDel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristGuideDel2Application.class, args);
+    }
+
+}
---
+++ TuristGuide-Del-2/src/test/java/com/example/turistguidedel2/TuristGuideDel2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turistguidedel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristGuideDel2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
