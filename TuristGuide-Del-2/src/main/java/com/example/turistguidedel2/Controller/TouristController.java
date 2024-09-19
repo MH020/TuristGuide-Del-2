a7211dc83a7aafeea3b09e1052095d392d48fe0d
MH020 <166133291+MH020@users.noreply.github.com>
2024-09-19 11:20:22 +0200

    added tilføjet save metode

--- TuristGuide-Del-2/src/main/java/com/example/turistguidedel2/Controller/TouristController.java
+++ TuristGuide-Del-2/src/main/java/com/example/turistguidedel2/Controller/TouristController.java
@@ -19,9 +19,9 @@ public class TouristController {
 
     @GetMapping
     public String getAllTouristAttractions(Model model){ //ResponseEntity<List<TouristAttraction>> getAllTouristAttractions(){
-       List<TouristAttraction> attractions = touristService.getAllTouristAttractions();
-       model.addAttribute("allTouristAttractions", attractions);
-        return "attractionList"; 
+        List<TouristAttraction> attractions = touristService.getAllTouristAttractions();
+        model.addAttribute("allTouristAttractions", attractions);
+        return "attractionList";
     }
     @GetMapping("/{name}")
     public String getTouristAttractionByName(@PathVariable String name, Model model){
@@ -35,6 +35,17 @@ public class TouristController {
         return "tags";
     }
 
+    @PostMapping("/save")
+    public String saveTouristAttractions(@ModelAttribute TouristAttraction touristAttraction) {
+        touristService.saveTouristAttractions(touristAttraction);
+        return "redirect:/attractions";
+    }
+    @GetMapping("/add")
+    public String addTouristAttraction(Model model){
+        model.addAttribute("TouristAttraction", new TouristAttraction());
+        model.addAttribute("taglist", touristService.getTouristAttractionTags());
+        return "add";
+    }
     @PostMapping("/update")
     public String updateTouristAttraction(@ModelAttribute TouristAttraction touristAttraction){
         touristService.updateTouristAttraction(touristAttraction);
@@ -51,5 +62,6 @@ public class TouristController {
         touristService.deleteTouristAttraction(index);
         return "redirect:/attractions";
     }
-//still up to date 
+//still up to date
 }
+
